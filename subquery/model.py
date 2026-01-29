abadb4142bc79f70d8213dec05b648d9ac9b57ce
Scott R. Malley <scott.r.malley@gmail.com>
2026-01-29 14:57:36 +0100

    refactor to remove liquidation & bankruptcy

--- subquery/model.py
+++ subquery/model.py
@@ -1,17 +1,13 @@
 from dataclasses import dataclass
 from eth_typing import ChecksumAddress
 
-@dataclass(frozen=True)
-class Account:
-    account_id: ChecksumAddress
-    margin_account: ChecksumAddress
-    collateral_asset: ChecksumAddress
 
 @dataclass(frozen=True)
 class AccountInfo:
     account: ChecksumAddress
     margin_account_address: ChecksumAddress
     quantity: int
+
 
 @dataclass(frozen=True)
 class ProductInfo:
